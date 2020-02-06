993c21512df037668bc46780cde010e5ed23784a
LypkaB <lpk.bdn@gmail.com>
2020-02-06 08:46:38 +0200

    Created section «Search form»

--- wp-content/themes/travel-cream/front-page.php
+++ wp-content/themes/travel-cream/front-page.php
@@ -7,20 +7,38 @@ Template Name: Home
 <?php get_header(); ?>
 
         <!-- Banner -->
-        <div class="banner">
+        <section class="banner">
             <div class="container">
                 <?php
-                    if (have_posts()) :
-                        while (have_posts()) : the_post();
+                    if ( have_posts() ) :
+                        while ( have_posts() ) : the_post();
                             the_content();
                         endwhile;
                     endif;
                 ?>
             </div>
             <!-- /.container -->
-        </div>
-        <!-- /.banner -->
+        </section>
     </div>
     <!-- /.header-bg -->
+
+    <!-- Search form -->
+    <section class="search-form">
+        <div class="container">
+            <form action="/" method="get">
+                <label for="from" class="from">
+                    <input type="text" id="from" placeholder="<?php the_field( 'input_from' ); ?>" value="<?php the_search_query(); ?>" />
+                </label>
+                <label for="wantGo" class="want-go">
+                    <input type="text" id="wantGo" placeholder="<?php the_field( 'input_want_go' ); ?>" value="<?php the_search_query(); ?>" />
+                </label>
+                <label for="date" class="date">
+                    <input type="text" id="date" placeholder="<?php the_field( 'input_date' ); ?>" value="<?php the_search_query(); ?>" />
+                </label>
+                <button type="submit" class="search-btn"><?php the_field( 'button_search' ); ?></button>
+            </form>
+        </div>
+        <!-- /.container -->
+    </section>
 
 <?php get_footer(); ?>
